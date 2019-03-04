dd7129b110027846519d2a63bede04fc5a9fc33d
Yuliang Jin <jyl198803@gmail.com>
2019-03-04 22:50:00 +0800

    finish up worker

--- worker/worker.rb
+++ worker/worker.rb
@@ -2,8 +2,9 @@ require 'redis'
 
 redis = Redis.new(host: 'redis', port: 6379, db: 0)
 
-# while true
-    task = redis.brpop('githook')
-    puts "Received Task: #{task}"
-    system('sh update.sh >> logs/update.log 2>&1')
+while true
+  task = redis.brpop('githook')
+  puts "Received Task: #{task}"
+  system('sh update.sh >> logs/update.log 2>&1')
+end
 
